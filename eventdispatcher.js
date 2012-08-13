47fa256974e7ed8568fdfe9c07b9bc76957f8973
Vidal Gonzalez <vidal@ooyala.com>
2012-08-13 14:38:58 -0500

    Refactor EventDispatcher with new OO syntax.

--- eventdispatcher.js
+++ eventdispatcher.js
@@ -1,40 +1,59 @@
 /**
 * Base class to all Objects that will support the "on('event', callback)" pattern for event listening
 * */
-Ooyala.Client.EventDispatcher = function(){
-  var eventHandlers = {};
 
-  var that = Object.create(Object.prototype);
-
-  that.on = function(eventName, eventHandler, context){
-    if(!eventHandlers[eventName]){                                            
-      eventHandlers[eventName] = [];                                          
-    }                                                                              
-
-    context = context || this;                                                     
-
-    eventHandlers[eventName].push({handler: eventHandler, context: context}); 
+(fnction(){
+  Ooyala.Client.EventDispatcher = function(){
+    this.eventHandlers = {};
   };
 
-  that.dispatchEvent = function(eventName){
-    var handlers = eventHandlers[eventName];   
+  $.extend(Ooyala.Client.EventDispatcher.prototype, {
+    on: function(eventName, eventHandler, context){
+      if(!this.eventHandlers[eventName]){
+        this.eventHandlers[eventName] = [];
+      }
 
-    if(!handlers){                                  
-      return;                                       
-    }                                               
+      context = context || this;
+      this.eventHandlers[eventName].push({handler: eventHandler, context: context});
+    },
 
-    var size = handlers.length;                     
+    detach: function(eventName, eventHandler){
+      var handlers = this.eventHandlers[eventName];
 
-    for(var i = 0; i < size; i++){                  
-      var h = handlers[i];                          
-      if(args){                                     
-        h.handler.apply(h.context, args);           
-      }                                             
-      else{                                         
-        h.handler.call(h.context);                  
-      }                                             
-    }                                             
-  };
+      if(!handlers) return;
 
-  return that;
-};
+      var size = handlers.length;
+      var indexToRemove = null;
+      for(var i = 0; i < size; i++){
+        if(handlers[i].handler === eventHandler){
+          indexToRemove = i;
+          break;
+        }
+      }
+
+      //Get rid of the desired handler
+      if(indexToRemove != null){
+        this.eventHandlers[eventName].splice(indexToRemove,1);
+      }
+    },
+
+    dispatchEvent: function(eventName, args){
+      var handlers = this.eventHandlers[eventName];
+
+      if(!handlers) return;
+
+      var size = handlers.length;
+      for(var i = 0; i < size; i++){
+        var h = handlers[i];
+        //Could happen when an event is trying to be dispatched and the handlers has been removed.
+        if(!h) continue;
+        if(args){
+          h.handler.apply(h.context, args);
+        }
+        else{
+          h.handler.call(h.context);
+        }
+      }
+    }
+  });
+}).call(this);
