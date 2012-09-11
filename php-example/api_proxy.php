6e2b399a848ec368e9467e4840eb192b7de42531
Andrew Berry <andrew.berry@lullabot.com>
2012-09-11 16:12:10 -0400

    Initial PHP example refactoring.
    Currently creates assets but does not implement uploading complete
    notifications yet.

--- php-example/api_proxy.php
+++ php-example/api_proxy.php
@@ -10,40 +10,89 @@ require 'OoyalaApi.php';
 define("OOYALA_API_KEY", "");
 define("OOYALA_API_SECRET", "");
 
+/**
+ * Parse out the path of the object being accessed. Ensures that we are posting
+ * a request and on the assets path. Your application should have something
+ * much more robust and sane than this.
+ *
+ * @param $path_info
+ *   A string containg the path being requested, with the leading slash.
+ *
+ * @return
+ *   An array of the path components, or FALSE if the path could not be parsed.
+ */
+function parsePath($path_info) {
+  $path = explode('/', substr($_SERVER['PATH_INFO'], 1));
+  if ($_SERVER['REQUEST_METHOD'] != 'POST' || $path[0] != 'assets') {
+    return FALSE;
+  }
+  return $path;
+}
+
+/**
+ * Kill this request with a 403.
+ */
+function http403() {
+  header("HTTP/1.1 403 Access denied");
+  exit;
+}
+
+/**
+ * Kill this request with a 500.
+ *
+ * @param $e
+ *   The exception that caused this request to fail.
+ */
+function http500($e) {
+  header("Status: 500 Internal Server Error");
+  error_log($e->getMessage());
+  exit;
+}
+
+// End of functions, begin our script here.
+if (!isset($_SERVER['PATH_INFO']) || !$path = parsePath($_SERVER['PATH_INFO'])) {
+  http403();
+}
+
 $api = new OoyalaApi(OOYALA_API_KEY, OOYALA_API_SECRET);
 
-$method = $_POST["method"];
-$parameters = json_decode($_POST["query_params"]);
-if($parameters == null){
-  $parameters = array();
-}
-$body = json_decode($_POST["body"]);
+// We can't use $_POST since that only works if we are posting urlencoded data
+// and not pure JSON.
+$clientAsset = json_decode(file_get_contents("php://input"));
 
-$path = $_POST["path"];
+if (count($path) == 1) {
+  // Build our request to create the new asset.
+  $asset = new stdClass();
+  $properties = array(
+    'name',
+    'description',
+    'file_name',
+    'file_size',
+    'chunk_size',
+    'asset_type',
+  );
+  foreach ($properties as $property) {
+    if (!isset($clientAsset->$property)) {
+      header("HTTP/1.1 403 Access denied");
+      exit;
+    }
+    $asset->{$property} = $clientAsset->{$property};
+  }
 
-try{
-  switch($method){
-    case "GET": 
-      echo json_encode($api->get($path, $parameters));
-      break;
-    case "POST": 
-      echo json_encode($api->post($path, $body, $parameters));
-      break;
-    case "PUT": 
-      echo json_encode($api->put($path, $body, $parameters));
-      break;
-    case "PATCH":
-      echo json_encode($api->patch($path, $body ,$parameters));
-      break;
-    case "DELETE": 
-      echo json_encode($api->delete($path));
-      break;
-    default:
-      echo "";
+  try {
+    $asset = $api->post("assets", $asset);
+    $uploading_urls = $api->get("assets/" . $asset->embed_code . "/uploading_urls");
+
+    $response = new stdClass();
+    $response->embed_code = $asset->embed_code;
+    $response->uploading_urls = $uploading_urls;
+    exit(json_encode($response));
+  }
+  catch(Exception $e){
+    http500($e);
   }
 }
-catch(Exception $e){
-  header("Status: 500 Internal Server Error");
-  echo $e->getMessage();
+else {
+  $embed_code = $path[1];
 }
-?>
+
